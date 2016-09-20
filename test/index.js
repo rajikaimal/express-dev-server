11265bdb110433c965d854757e3206b79eaab91a
rajikaimal <inbox.rajika@gmail.com>
2016-09-20 22:16:58 +0530

    additional tests

--- test/index.js
+++ test/index.js
@@ -5,9 +5,16 @@ const devServer = require('../index');
 describe('express-dev-server-api', function () {
 	it('start dev server', function (done) {
 		shelljs.exec('node index.js 3001 src', function(code, stdout, stderr) {
-			console.log(stdout);
-			assert.equal(stdout, `express dev server running on port 3001, serving src`);
+			assert.equal(stdout, 'express dev server running on port 3001, serving src\n');
 			done();
-		});	
+		});
+		shelljs.exec('node index.js', function(code, stdout, stderr) {
+			assert.equal(stdout, 'provide web server port !\n');
+			done();
+		});
+		shelljs.exec('node index.js 3001', function(code, stdout, stderr) {
+			assert.equal(stdout, 'provide static file directory !\n');
+			done();
+		});
 	});
 });
